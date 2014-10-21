60d258dc20309e37eff42b3be3f28e06c9808c56
Zachary Adam Kaplan <razic@viralkitty.com>
2014-10-21 18:54:17 +0000

    API#logged_in?

--- lib/okcupid/api.rb
+++ lib/okcupid/api.rb
@@ -1,4 +1,17 @@
 module OkCupid
-  module API
+  class API
+    attr_accessor :authorization_token
+
+    # Public: Determines if the user is logged in.
+    #
+    # Examples
+    #
+    #   logged_in?
+    #   # => true
+    #
+    # Returns true or false.
+    def logged_in?
+      !!authorization_token
+    end
   end
 end
--- spec/okcupid/api_spec.rb
+++ spec/okcupid/api_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe OkCupid::API do
+  let(:api) { OkCupid::API.new }
+
+  describe '#logged_in?' do
+    subject { api.logged_in? }
+
+    context 'when an authorization token is available' do
+      before(:each) { api.authorization_token = true }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when an authorization token is not available' do
+      it { is_expected.to be false }
+    end
+  end
 end
